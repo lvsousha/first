8de35ce8710f4e87aebf84b040651c75da857282
lvsousha <zclvideo@163.com>
2015-12-16 23:55:16 +0800

    FileUtil   遍历某个路径的所有文件

--- first/src/main/tool/cn/zcl/action/fileutil/FileUtil.java
+++ first/src/main/tool/cn/zcl/action/fileutil/FileUtil.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 import javax.servlet.http.HttpServletRequest;
@@ -73,4 +74,96 @@ public class FileUtil {
 	        }		
 	      return null;
 	}
+
+	/**
+	 * 非递归方法遍历某个路径的文件
+	 * @param path 路径
+	 */
+	public static void traverseFolder1(String path) {
+        int fileNum = 0, folderNum = 0;
+        File file = new File(path);
+        if (file.exists()) {
+            LinkedList<File> list = new LinkedList<File>();
+            File[] files = file.listFiles();
+            for (File file2 : files) {
+                if (file2.isDirectory()) {
+                    System.out.println("文件夹:" + file2.getAbsolutePath());
+                    list.add(file2);
+                    fileNum++;
+                } else {
+                    System.out.println("文件:" + file2.getAbsolutePath());
+                    folderNum++;
+                }
+            }
+            File temp_file;
+            while (!list.isEmpty()) {
+                temp_file = list.removeFirst();
+                files = temp_file.listFiles();
+                for (File file2 : files) {
+                    if (file2.isDirectory()) {
+                        System.out.println("文件夹:" + file2.getAbsolutePath());
+                        list.add(file2);
+                        fileNum++;
+                    } else {
+                        System.out.println("文件:" + file2.getAbsolutePath());
+                        folderNum++;
+                    }
+                }
+            }
+        } else {
+            System.out.println("文件不存在!");
+        }
+        System.out.println("文件夹共有:" + folderNum + ",文件共有:" + fileNum);
+    }
+	
+	/**
+	 * 采用递归的方法遍历文件
+	 * @param path 路径
+	 */
+	public static void traverseFolder2(String path) {
+
+        File file = new File(path);
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            if (files.length == 0) {
+                System.out.println("文件夹是空的!");
+                return;
+            } else {
+                for (File file2 : files) {
+                    if (file2.isDirectory()) {
+                        System.out.println("文件夹:" + file2.getAbsolutePath());
+                        traverseFolder2(file2.getAbsolutePath());
+                    } else {
+                        System.out.println("文件:" + file2.getAbsolutePath());
+                    }
+                }
+            }
+        } else {
+            System.out.println("文件不存在!");
+        }
+    }
+	
+	/**
+	 * 获取该路径下所有为该后缀的文件
+	 * @param strPath 路径
+	 * @param suffix 后缀(.avi)
+	 */
+	public static void getFileList(String strPath, String suffix) {
+        File dir = new File(strPath);
+        File[] files = dir.listFiles(); // 该文件目录下文件全部放入数组
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getName();
+                if (files[i].isDirectory()) { // 判断是文件还是文件夹
+                    getFileList(files[i].getAbsolutePath(), suffix); // 获取文件绝对路径
+                } else if (fileName.endsWith(suffix)) { // 判断文件名是否以.avi结尾
+                    String strFileName = files[i].getAbsolutePath();
+                    System.out.println("---" + strFileName);
+                } else {
+                    continue;
+                }
+            }
+
+        }
+    }
 }
--- first/src/main/tool/cn/zcl/action/fileutil/Main.java
+++ first/src/main/tool/cn/zcl/action/fileutil/Main.java
@@ -5,8 +5,7 @@ package cn.zcl.action.fileutil;
 public class Main {
 
 	public static void main(String[] args) {
-		String str = FileUtil.firstToUpcase("a");
-		System.out.println(str);
+		FileUtil.traverseFolder1("E:\\");
 	}
 
 }
