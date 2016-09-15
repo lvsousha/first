f6603c569b31b07b6784bc6d15fbbdab52968c8a
lvsousha <zclvideo@163.com>
2016-09-15 12:11:08 +0800

    提交

--- first/src/main/tool/cn/zcl/action/password/Encryption.java
+++ first/src/main/tool/cn/zcl/action/password/Encryption.java
@@ -1,10 +1,22 @@
 package cn.zcl.action.password;
 
+import java.lang.reflect.Method;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.eclipse.jetty.util.security.Credential.MD5;
 
 public class Encryption {
+	
+	private static final String HMAC_SHA1 = "HmacSHA1";     
+    private static final String ENCODING = "UTF-8";
 
 	public static String base64(String str){
 		String base64 = Base64.encodeBase64String(str.getBytes());
@@ -23,13 +35,99 @@ public class Encryption {
 		System.out.println(sha);
 		return sha;
 	}
+	
+	public static byte[] getSignature(byte[] data, byte[] key) throws InvalidKeyException, NoSuchAlgorithmException {  
+        SecretKeySpec signingKey = new SecretKeySpec(key, HMAC_SHA1);  
+        Mac mac = Mac.getInstance(HMAC_SHA1);  
+        mac.init(signingKey);  
+        byte[] rawHmac = mac.doFinal(data);  
+        return rawHmac;  
+    }
 
-	public static void main(String[] args) {
+	/** 
+     * 获取MD5 结果字符串 
+     *  
+     * @param source 
+     * @return 
+     */  
+    public static String encode(byte[] source) {  
+        String s = null;  
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };  
+        try {  
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");  
+            md.update(source);  
+            byte tmp[] = md.digest();   
+            char str[] = new char[16 * 2];   
+            int k = 0;   
+            for (int i = 0; i < 16; i++) {   
+                byte byte0 = tmp[i];   
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];   
+                str[k++] = hexDigits[byte0 & 0xf];   
+            }  
+            s = new String(str);   
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        return s;  
+    }  
+      
+    public static String getMD5(String source) {  
+        return (source == null || "".equals(source)) ? "" : getMD5(source);  
+    }
+    
+    public static String encodeBase64(byte[]input) throws Exception{  
+        Class<?> clazz=Class.forName("com.sun.org.apache.xerces.internal.impl.dv.util.Base64");  
+        Method mainMethod= clazz.getMethod("encode", byte[].class);  
+        mainMethod.setAccessible(true);  
+         Object retObj=mainMethod.invoke(null, new Object[]{input});  
+         return (String)retObj;  
+    }
+    
+    public static String SHA1(String decript) {
+		try {
+			MessageDigest digest = java.security.MessageDigest
+					.getInstance("SHA-1");
+			digest.update(decript.getBytes());
+			byte messageDigest[] = digest.digest();
+			// Create Hex String
+			StringBuffer hexString = new StringBuffer();
+			// 字节数组转换为 十六进制 数
+			for (int i = 0; i < messageDigest.length; i++) {
+				String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+				if (shaHex.length() < 2) {
+					hexString.append(0);
+				}
+				hexString.append(shaHex);
+			}
+			return hexString.toString();
+
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+    
+	public static void main(String[] args) throws Exception {
 //		Encryption e = new Encryption();
 //		Encryption.base64("hello");
 //		Encryption.hex("hello");
 //		Encryption.md5("hello", "123");
-		Encryption.md5("hello", "abc");
+//		https://api.thinkpage.cn/v3/weather/now.json?location=beijing&ts=1473321996&ttl=3000&uid=U1D39FC8C8&sig=Tn1oDyU8uCaV8ZbImG5FGU90te8=
+		String data = "ts=1473321996&ttl=3000&uid=U1D39FC8C8";
+		String key = "isxsnikgqomz9sxv";
+		String datas = "ts=1443079775&ttl=30&uid=U123456789";
+		String keys = "secret";
+		String test = "dTYeoN8WdOfW4PiwgEdLa0gWFzo=";
+		try {
+			byte[] encryData = Encryption.getSignature(data.getBytes(ENCODING), key.getBytes(ENCODING));
+			String encode = Encryption.encodeBase64(encryData);
+			System.out.println(encode);
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- first/src/main/tool/cn/zcl/action/regex/RegexUtil.java
+++ first/src/main/tool/cn/zcl/action/regex/RegexUtil.java
@@ -1,44 +1,33 @@
 package cn.zcl.action.regex;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtil {
 
-	public void regexTest(){
-		Pattern pattern = Pattern.compile("^Java.*");
-		Matcher matcher = pattern.matcher("Java 是一门编程语言");
-		boolean b = matcher.matches();
-		System.out.println("查找已Java开头，任意结尾的字符串："+b);
-		//
-		pattern = Pattern.compile("\\{\\d*\\}");
-		matcher = pattern.matcher("{2323}");
-		System.out.println(matcher.replaceFirst("Java"));
-		//
-		System.out.println("已多个条件分割字符串");
-		pattern = Pattern.compile("[, |]+");
-		String [] strs = pattern.split("Java Hello World  Java,Hello,,World|Sun");
-		for(String str : strs){
-			System.out.print(str+"--");
-		}
-		System.out.println("\n替换第一个符合正则表达式的数据");
-		pattern = Pattern.compile("正则表达式");
-		matcher = pattern.matcher("正则表达式 Hello World,正则表达式 Hello World");
-		System.out.println(matcher.replaceFirst("Java"));
-		System.out.println("文字替换（置换字符）");
-		matcher = pattern.matcher("正则表达式 Hello World,正则表达式 Hello World");
-		StringBuffer sbr = new StringBuffer();
-		while(matcher.find()){
-			matcher.appendReplacement(sbr, "Java");
-		}
-		matcher.appendTail(sbr);
-		System.out.println(sbr.toString());
+	public List<String> getFirstMatchContenet(String orign,String regex){
+		List<String> ls=new ArrayList<String>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(orign);
+        if(matcher.find()){
+        	ls.add(matcher.group());
+        }
+        return ls;
 	}
 	
 	
 	public static void main(String[] args) {
 		RegexUtil ru = new RegexUtil();
-		ru.regexTest();
+		String orign = "&amp;amp;lt;p&amp;amp;gt;&amp;amp;#xA;&amp;amp;lt;title&amp;amp;gt;&amp;amp;lt;/title&amp;amp;gt;&amp;amp;#xA;&amp;amp;lt;/p&amp;amp;gt;&amp;amp;#xA;&amp;amp;#xA;";
+//		String regex = "（20\\d{2}）(\\W)+(\\d)+号";
+//		List<String> matchs = ru.getFirstMatchContenet(orign, regex);
+//		for(String match : matchs){
+//			System.out.println(match);
+//		}
+		System.out.println(orign.replaceAll("[[amp;][lt;][gt;][#xA;]]", ""));
+		
 	}
 
 }
